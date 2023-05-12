28f355d9e4b3d84576fbcab74a6f7426a197e8e6
juanarfuch <juan.arfuch@gmail.com>
2023-05-12 06:15:24 -0300

    coming back

--- app.py
+++ app.py
@@ -38,10 +38,9 @@ with header:
 # Wrap transcript loading and splitting in try/except block
 try:
     if not st.session_state["video_loaded"]:
-        with st.form('Load video'):
-            st.session_state["video_url"] = st.text_input("Please enter the YouTube video URL: ")
-            submitted = st.form_submit_button("Load Video")
-            if submitted:
+        st.session_state["video_url"] = st.text_input("Please enter the YouTube video URL: ")
+        if st.button('Load video'):
+            if st.session_state["video_url"]:
                 with st.spinner('Loading video transcript...'):
                     transcript = load_transcript(st.session_state["video_url"])
                     docs = split_transcript(transcript)
